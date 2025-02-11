a8e95e09359e8c5beaca0a5b63196e097168c578
Raisaguzman <raisa1005@gmail.com>
2025-02-11 14:01:18 -0500

    corrección en precioUnitario y el length

--- src/main/java/com/dgitalhouse/integradorBackend/entity/Producto.java
+++ src/main/java/com/dgitalhouse/integradorBackend/entity/Producto.java
@@ -11,13 +11,16 @@ public class Producto {
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         private Long id;
-        @Column(length = 30, nullable = false)
-        private String nombre;
         @Column(length = 100, nullable = false)
+        private String nombre;
+        @Column(length = 200, nullable = false)
         private String descripcion;
 
+        @Column(length = 255, nullable = false)
+        private String imagen;
+
         @Column(length = 100, nullable = false)
-        private String precioUnitario;
+        private Double precioUnitario;
 
     }
 }

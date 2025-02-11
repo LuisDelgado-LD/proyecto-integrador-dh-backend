29fc3990a275773ed3f7e997482b380b6a85f4b0
Raisaguzman <raisa1005@gmail.com>
2025-02-11 11:33:02 -0500

    estructura general del proyecto

---
+++ src/test/java/com/dgitalhouse/integradorBackend/IntegradorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgitalhouse.integradorBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegradorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

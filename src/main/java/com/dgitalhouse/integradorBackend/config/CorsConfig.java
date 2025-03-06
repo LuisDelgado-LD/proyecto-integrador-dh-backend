ce10328db87907844d1608b7d307173400ebc5c9
Raisaguzman <raisa1005@gmail.com>
2025-03-06 14:19:53 -0500

    cors config

---
+++ src/main/java/com/dgitalhouse/integradorBackend/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.dgitalhouse.integradorBackend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*") // Cambia "*" por tu dominio si es necesario
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
+
+
